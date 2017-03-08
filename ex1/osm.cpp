a3efcae999edbd2c556a8a01a2f1e0761a83ee15
Jenia <genia90@gmail.com>
2017-03-08 17:10:57 +0200

    initial ex1 commit

---
+++ ex1/osm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jenia90 on 3/8/17.
+//
+
+#include "osm.h"
---
+++ ex1/osm.h
@@ -0,0 +1,14 @@
+//
+// Created by jenia90 on 3/8/17.
+//
+
+#ifndef PROJECT_OSM_H
+#define PROJECT_OSM_H
+
+
+class osm {
+
+};
+
+
+#endif //PROJECT_OSM_H
