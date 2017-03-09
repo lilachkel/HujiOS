d14737753c45f6deae4da3c8877cd572a17de100
Jenia <genia90@gmail.com>
2017-03-09 10:55:29 +0200

    created osm.cpp function definitions

--- ex1/osm.cpp
+++ ex1/osm.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "osm.h"
+
+int osm_init()
+{
+    return 0;
+}
+
+int osm_finalizer()
+{
+    return 0;
+}
+
+double osm_operation_time(unsigned int iterations)
+{
+    return 0;
+}
+
+double osm_function_time(unsigned int iterations)
+{
+    return 0;
+}
+
+double osm_syscall_time(unsigned int iterations)
+{
+    return 0;
+}
+
+double osm_disk_time(unsigned int iterations)
+{
+    return 0;
+}
+
+timeMeasurmentStructure
+measureTimes(unsigned int operation_iterations, unsigned int function_iterations,
+             unsigned int syscall_iterations, unsigned int disk_iterations)
+{
+    return timeMeasurmentStructure();
+}
