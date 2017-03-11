e5da6e4f10f71621eebda1671a4116f89af73a80
Jenia <genia90@gmail.com>
2017-03-11 16:52:14 +0200

    merged changes and extracted repeating code to a separate function.

--- ex1/osm.cpp
+++ ex1/osm.cpp
@@ -2,7 +2,9 @@
 // Created by jenia90 on 3/8/17.
 //
 
-#include <zconf.h>
+#include <cstdio>
+#include <io.h>
+#include <winsock.h>
 #include "osm.h"
 #include "sys/time.h"
 int osm_init()
@@ -15,18 +17,17 @@ int osm_finalizer()
     return 0;
 }
 
-void dummyFunc(){}
-
-double osm_operation_time(unsigned int iterations)
+template <typename func>
+double measureRuntime(func op, unsigned int iterations)
 {
     struct timeval s, e;
     int i = 0;
 
     double timemeasure = 0;
-    for (i; i<iterations ; i++)
+    for (i; i < iterations ; i++)
     {
         gettimeofday(&s, NULL);
-        1+1;
+        op();
         gettimeofday(&e, NULL);
         timemeasure += e.tv_usec - s.tv_usec;
     }
@@ -34,65 +35,37 @@ double osm_operation_time(unsigned int iterations)
     {
         return (timemeasure * 1000) / iterations;
     }
+
     return -1;
+}
+
+int dummyFunc(){ return 0; }
+
+double osm_operation_time(unsigned int iterations)
+{
+    return measureRuntime([] ->
+                          { 1 + 1; }, iterations);
 }
 
 double osm_function_time(unsigned int iterations)
 {
-    struct timeval s, e;
-    double diff = 0;
-
-    for(int i = 0; i < iterations; i++)
-    {
-        gettimeofday(&s, nullptr);
-        dummyFunc();
-        gettimeofday(&e, nullptr);
-        diff += e.tv_usec - s.tv_usec;
-    }
-
-    return (diff * 1000) / iterations;
+    return measureRuntime(dummyFunc, iterations);
 }
 
 double osm_syscall_time(unsigned int iterations)
 {
-    struct timeval s, e;
-    int i = 0;
-
-    double timemeasure = 0;
-    for (i; i<iterations ; i++)
-    {
-        gettimeofday(&s, NULL);
-        OSM_NULLSYSCALL;
-        gettimeofday(&e, NULL);
-        timemeasure += e.tv_usec-s.tv_usec;
-    }
-    if(timemeasure != 0)
-    {
-        return (timemeasure*1000)/iterations;
-    }
-    return -1;
-
+    return measureRuntime([] ->
+                          { OSM_NULLSYSCALL; }, iterations);
 }
 
 double osm_disk_time(unsigned int iterations)
 {
-    struct timeval s, e;
-    int i = 0;
-
-
-    double timemeasure = 0;
-    for (i; i<iterations ; i++)
-    {
-        gettimeofday(&s, NULL);
-        fopen("someFile", "w");
-        gettimeofday(&e, NULL);
-        timemeasure += e.tv_usec-s.tv_usec;
-    }
-    if(timemeasure != 0)
-    {
-        return (timemeasure*1000)/iterations;
-    }
-    return -1;
+    // TODO: Finish implementation of this lambda function.
+    return measureRuntime([] ->
+                          {
+                              int id = open("test.txt", 1);
+                              read(id,);
+                          }, iterations);
 
 }
 
@@ -102,7 +75,6 @@ timeMeasurmentStructure measureTimes(unsigned int operation_iterations,
                                      unsigned int disk_iterations)
 {
     timeMeasurmentStructure time_s;
-    char *a = "blah";
     double  iTime = osm_operation_time(operation_iterations),
             fTime = osm_function_time(function_iterations),
             dTime = osm_disk_time(disk_iterations),
@@ -111,7 +83,7 @@ timeMeasurmentStructure measureTimes(unsigned int operation_iterations,
             idTime = iTime == 0 || dTime == 0 ? 0 : iTime / dTime,
             isTime = iTime == 0 || sTime == 0 ? 0 : iTime / sTime;
 
-    gethostname(time_s.machineName, 100);
+    gethostname(time_s.machineName, 255);
     time_s.functionInstructionRatio = ifTime;
     time_s.functionTimeNanoSecond = fTime;
     time_s.diskTimeNanoSecond = dTime;
