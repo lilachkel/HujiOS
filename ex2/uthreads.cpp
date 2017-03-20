db9bceeb9f5372e68425d06f2acf28866a453f9b
Jenia <genia90@gmail.com>
2017-03-20 19:59:43 +0200

    created uthreads definitions

--- ex2/uthreads.cpp
+++ ex2/uthreads.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "uthreads.h"
+
+int uthread_init(int quantum_usecs) {
+    return 0;
+}
+
+int uthread_spawn(void (*f)(void)) {
+    return 0;
+}
+
+int uthread_terminate(int tid) {
+    return 0;
+}
+
+int uthread_block(int tid) {
+    return 0;
+}
+
+int uthread_resume(int tid) {
+    return 0;
+}
+
+int uthread_sync(int tid) {
+    return 0;
+}
+
+int uthread_get_tid() {
+    return 0;
+}
+
+int uthread_get_total_quantums() {
+    return 0;
+}
+
+int uthread_get_quantums(int tid) {
+    return 0;
+}
